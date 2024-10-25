47003e23fb18d722f7966d4074af84320e1d81da
Sahil8709 <sahilraza87097@gmail.com>
2024-10-26 00:23:35 +0530

    Everything added

---
+++ src/com/nt/comps/BlueDart.java
@@ -0,0 +1,14 @@
+package com.nt.comps;
+
+public final class BlueDart implements ICourier {
+
+	@Override
+	public String deliver(int Oid) {
+		
+		return Oid+"order id order is assigned to BlueDart for delivary";
+		
+		
+	}
+
+}
+
